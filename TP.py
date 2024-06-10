8518575b7a6fa33b6d0ee8b6d15ae0099c296491
Heartswimm <valentinblabla@laposte.net>
2024-06-10 09:02:08 +0200

    Update TP.py

--- TP.py
+++ TP.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #1. Importation du jeu de données
-csvfile = pd.read_csv('Data/Phising_dataset_predict.csv', delimiter = ';')
+csvfile = pd.read_csv('./Data/Phising_dataset_predict.csv', delimiter = ';')
 
 csvfile.head(5)
 
