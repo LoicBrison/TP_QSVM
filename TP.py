beff5a4337ccbddaf6b68858df87f37677570be4
Heartswimm <valentinblabla@laposte.net>
2024-06-10 09:01:30 +0200

    Update TP.py

--- TP.py
+++ TP.py
@@ -6,3 +6,14 @@ csvfile = pd.read_csv('Data/Phising_dataset_predict.csv', delimiter = ';')
 csvfile.head(5)
 
 csvfile.info
+
+#Questions guidantes :
+# • Comment importer un fichier CSV en Python ?
+# • Quelles informations peut-on obtenir avec les méthodes head() et info() de pandas ?
+
+# Head = Les première ligne du tableau tout dépend du chiffre entre parenthèse
+# Info = 
+
+
+
+#2. Visualisation du jeu de données :
